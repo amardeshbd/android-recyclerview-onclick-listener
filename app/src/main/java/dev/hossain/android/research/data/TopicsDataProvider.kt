d0a6b28ef8782cd8848cdd03e2cb98d824976b61
Hossain Khan <amardeshbd@gmail.com>
2020-07-01 02:22:45 -0400

    [UPDATE] Applied some style tweaks and cleanup

--- app/src/main/java/dev/hossain/android/research/data/TopicsDataProvider.kt
+++ app/src/main/java/dev/hossain/android/research/data/TopicsDataProvider.kt
@@ -5,14 +5,17 @@ import dev.hossain.android.research.data.model.ResearchTopic
 object TopicsDataProvider {
     val topics = listOf(
         ResearchTopic(
-            title = "Plain Old Recycler View Listener",
-            description = "",
+            title = "Plain old listener on ViewHolder",
+            description = "This is example of setting plain click listener using interface. " +
+                    "This reference example was taken from XYZ app.",
             url = null
         ),
         ResearchTopic(
-            title = "Databinding adapter listener",
-            description = "",
-            url = null
+            title = "Modern listener using DataBinding",
+            description = "This is the example taken from Google's Sunflower app that uses data binding to " +
+                    "set model data on view holder and retrieve it when click listener is invoked. " +
+                    "This is also taught in the Udacity 'Developing Android Apps with Kotlin' course.",
+            url = "https://github.com/android/sunflower/blob/master/app/src/main/java/com/google/samples/apps/sunflower/adapters/PlantAdapter.kt#L46"
         )
     )
 }
