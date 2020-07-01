7a0d64144ad32dc92b70aeffc1db51a6fa1233df
Hossain Khan <amardeshbd@gmail.com>
2020-07-01 02:07:20 -0400

    [ADDED] More list item styling with icon

--- app/src/main/java/dev/hossain/android/research/data/TopicsDataProvider.kt
+++ app/src/main/java/dev/hossain/android/research/data/TopicsDataProvider.kt
@@ -4,7 +4,15 @@ import dev.hossain.android.research.data.model.ResearchTopic
 
 object TopicsDataProvider {
     val topics = listOf(
-        ResearchTopic("Plain Old Recycler View Listener"),
-        ResearchTopic("Databinding adapter listener")
+        ResearchTopic(
+            title = "Plain Old Recycler View Listener",
+            description = "",
+            url = null
+        ),
+        ResearchTopic(
+            title = "Databinding adapter listener",
+            description = "",
+            url = null
+        )
     )
 }
--- app/src/main/java/dev/hossain/android/research/data/model/ResearchTopic.kt
+++ app/src/main/java/dev/hossain/android/research/data/model/ResearchTopic.kt
@@ -1,5 +1,10 @@
 package dev.hossain.android.research.data.model
 
+/**
+ * Data class for different research topic related to `RecyclerView` item click listener.
+ */
 data class ResearchTopic constructor(
-    val title: String
+    val title: String,
+    val description: String,
+    val url: String? = null
 )
