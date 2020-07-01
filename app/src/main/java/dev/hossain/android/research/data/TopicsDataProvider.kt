a6ccfa606e1ef49c3c6c857b0802b84d400dfc87
Hossain Khan <amardeshbd@gmail.com>
2020-07-01 02:47:47 -0400

    [ADDED] Topic ID to easily use for navigation event

--- app/src/main/java/dev/hossain/android/research/data/TopicsDataProvider.kt
+++ app/src/main/java/dev/hossain/android/research/data/TopicsDataProvider.kt
@@ -6,14 +6,19 @@ import dev.hossain.android.research.data.model.ResearchTopic
  * Provides list of [ResearchTopic] for adapter.
  */
 object TopicsDataProvider {
+    const val TYPE_DATA_PLAIN_LISTENER = "EXAMPLE_ITEM_PLAIN_LISTENER"
+    const val TYPE_DATA_BINDING_ASSISTED = "EXAMPLE_ITEM_DATA_BINDING_ASSISTED"
+
     val topics = listOf(
         ResearchTopic(
+            id = TYPE_DATA_PLAIN_LISTENER,
             title = "Plain old listener on ViewHolder",
             description = "This is example of setting plain click listener using interface. " +
                     "This reference example was taken from XYZ app.",
             url = null
         ),
         ResearchTopic(
+            id = TYPE_DATA_BINDING_ASSISTED,
             title = "Modern listener using DataBinding",
             description = "This is the example taken from Google's Sunflower app that uses data binding to " +
                     "set model data on view holder and retrieve it when click listener is invoked. " +
--- app/src/main/java/dev/hossain/android/research/data/model/ResearchTopic.kt
+++ app/src/main/java/dev/hossain/android/research/data/model/ResearchTopic.kt
@@ -4,6 +4,7 @@ package dev.hossain.android.research.data.model
  * Data class for different research topic related to `RecyclerView` item click listener.
  */
 data class ResearchTopic constructor(
+    val id: String,
     val title: String,
     val description: String,
     val url: String? = null
